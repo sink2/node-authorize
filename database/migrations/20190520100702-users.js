e2c7adef7dd0b28c59923d6fcb08970cd2aa9608
waisink15s@gmail.com <EASTfive@114>
2019-05-21 00:46:13 +0800

    Add `users` migration.

--- database/migrations/20190520100702-users.js
+++ database/migrations/20190520100702-users.js
@@ -9,7 +9,21 @@ module.exports = {
             Example:
             return queryInterface.createTable('users', { id: Sequelize.INTEGER });
         */
-        return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        const { UUID, UUIDV1, STRING, DATE, ENUM } = Sequelize;
+        return queryInterface.createTable(
+            'users',
+            {
+                id: { type: UUID, defaultValue: UUIDV1, primaryKey: true, allowNull: false },
+                name: { type: STRING(64), allowNull: false },
+                password: { type: STRING(256), allowNull: false },
+                description: STRING(256),
+                salt: STRING(256),
+                // timestamps
+                createdAt: DATE,
+                updatedAt: DATE,
+            },
+            { charset: 'UTF8' },
+        );
     },
 
     down: queryInterface => {
