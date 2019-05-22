b36115159e83ff6552ab6f6a3438a0a705743762
liujiali <liu_jiali1@Hoperun.com>
2019-05-22 16:13:10 +0800

    Update typings.

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -2,10 +2,12 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+import ExportValidateConfig from '../../../app/controller/validateConfig';
 import ExportV1Users from '../../../app/controller/v1/users';
 
 declare module 'egg' {
   interface IController {
+    validateConfig: ExportValidateConfig;
     v1: {
       users: ExportV1Users;
     }
