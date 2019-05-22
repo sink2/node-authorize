bfabff8b6c2a6bc59c5fea08cf7e697d7044afee
waisink15s@gmail.com <EASTfive@114>
2019-05-22 22:57:51 +0800

    Add users `update` controller.

--- app/controller/v1/users.ts
+++ app/controller/v1/users.ts
@@ -1,6 +1,6 @@
 import { Controller } from 'egg';
 import * as crypto from 'crypto';
-import { pick, omit } from 'lodash';
+import { pick, omit, isEmpty } from 'lodash';
 import { commonIndexRule } from '../validateConfig';
 
 const indexRule = {
@@ -17,10 +17,10 @@ const createRule = {
 };
 
 const updateRule = {
-    name: { type: 'string', max: 64 },
-    password: { type: 'string', max: 256 },
+    name: { type: 'string?', max: 64 },
+    password: { type: 'string?', max: 256 },
     description: { type: 'string?', max: 256 },
-}
+};
 
 const encryptPassword = (password: string, salt: string = crypto.randomBytes(32).toString()) => {
     const md5 = crypto.createHash('md5');
@@ -108,15 +108,42 @@ export default class UsersController extends Controller {
     public async update() {
         const { ctx } = this;
         const { service, model } = ctx;
-        ctx.validate(updateRule, ctx.request.query);
+        ctx.validate(updateRule, ctx.request.body);
         const { id } = ctx.params;
         try {
             const isExist = (await model.Users.count({ where: { id } })) > 0;
             if (!isExist) {
                 service.responseHelper.handleResponse(ctx, 404);
             } else {
-
-                service.responseHelper.handleResponse(ctx, 200);
+                const {name, password, description} = ctx.request.body;
+                const updateParams: any = {};
+                if (name) {
+                    updateParams.name = name;
+                }
+                if (password) {
+                    const salt = crypto.randomBytes(32).toString();
+                    const saltPassword = encryptPassword(password, salt);
+                    updateParams.salt = salt;
+                    updateParams.password = saltPassword;
+                }
+                if (description) {
+                    updateParams.description = description;
+                }
+                const data = await service.queryHelper.queryOne(model.Users, id);
+                const restParams = omit(ctx.request.body, ['name', 'password', 'description']);
+                if (!isEmpty(restParams)) {
+                    updateParams.additionalInfo = {
+                        ...JSON.parse(data.additionalInfo),
+                        ...restParams,
+                    };
+                }
+                await model.Users.update(updateParams, {where: { id }});
+                const result = await service.queryHelper.queryOne(model.Users, id, [], ['password', 'salt']);
+                const response = {
+                    ...JSON.parse(result.additionalInfo),
+                    ...omit(result, 'additionalInfo'),
+                };
+                service.responseHelper.handleResponse(ctx, 200, {data: response});
             }
         } catch (e) {
             ctx.logger.error(e);
